916819bac5e4eb463246978604a3dbeed3d63ede
Rafael Lima <rafael.deabreu.tjii@gmail.com>
2023-08-09 14:25:01 -0300

    AC - 1

--- ac1.go
+++ ac1.go
@@ -1,59 +1,98 @@
+/* Primeiros exercícios da AC de Estrutura de Dados
+
+Aluno: Rafael Lima
+
+*/
+
+
 package main
 
 import "fmt"
 
 func main(){
 
-	resultado1 := ehPrimo(7)
+	// Exercício 1 da AC - Basta trocar o número para ver se é primo
+	resultado1 := ePrimo(7)
 	fmt.Println(resultado1)
 
-	resultado2 := fibo(5)
-	fmt.Println(resultado2)
+	// Exercício 2 da AC - Basta trocar o número limite para descobrir o "n-ésimo"
+	resultado2 := fibo(4)
+	fmt.Printf("\nO n-ésimo número é %v!", resultado2)
 
-	resultado3 := diaSemana(9)
-	fmt.Println(resultado3)
+	// Exercício 3 da AC - Basta trocar o número do dia para sair o nome correspondente definido no GitHub da disciplina
+	resultado3 := diaSemana(7)
+	fmt.Printf("\nO dia da semana é %v!", resultado3)
+
+	// Exercício 4 da AC - Basta trocar o ano para definir se é bissexto ou não
+	resultado4 := eBissexto(1900)
+	fmt.Printf("\nO ano é %v!\n", resultado4)
 }
 
 
-func ehPrimo(z int) string{
+func ePrimo(num int) string{
 	var contador int = 0
-	for i := 2; i < z; i++{
-		if z % i == 0 {
+	for i := 2; i < num; i++{
+		if num % i == 0 {
 			contador++
 			fmt.Print(i, " - ")
 		}
 	}
 
 	if contador >= 1 {
-		return "Não primo"
+		return "O número não é primo!"
 	} else {
-		return "Primo"
+		return "O número é primo!"
 	}
 }
 
+func fibo(ex2 int) int {
+	var num1, num2 int = 1, 1
+	var nesimo int = 0
+	fmt.Print("Sequência: ")
+	for i := 0; i < ex2; i++ {
+		fmt.Print(num1, " ")
+		nesimo = num1
+		num1, num2 = num2, num1+num2
+	} 
+	return nesimo
+}
 
-func diaSemana(x int) string{
-	switch x{
+
+
+func diaSemana(dia int) string{
+	switch dia{
 		case 1:
-			return "Domingo"
+			return "domingo"
 		case 2:
-			return "Segunda-feira"
+			return "segunda-feira"
 		case 3:
-			return "Terça-feira"
+			return "terça-feira"
 		case 4:
-			return "Quarta-feira"		
+			return "quarta-feira"		
 		case 5:
-			return "Quinta-feira"
+			return "quinta-feira"
 		case 6:
-			return "Sexta-feira"
+			return "sexta-feira"
+		case 7:
+			return "sábado"
 		default:
-			return "Erro"	
+			return "não foi encontrado"	
 	}
 
 }
 
 
-func e_bissexto(ano int){
-	
-
+func eBissexto(ano int) string{
+	if ano % 4 == 0 {	
+		if ano % 100 == 0 && ano % 400 != 0 {
+			// fmt.Printf("\nO ano %v não é bissexto!\n", ano)
+			return "não bissexto"
+		} else {
+			// fmt.Printf("\nO ano %v é bissexto!\n", ano)
+			return "bissexto"
+		}
+	} else {
+		// fmt.Printf("\nO ano %v não é bissexto!\n", ano)
+		return "não bissexto"
+	}
 }
